5cb3963d83332db570f56846f5b40ec53036f6d6
haseeb usman <150597331+haseebusman0305@users.noreply.github.com>
2024-04-18 22:56:40 +0500

    updates issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{jsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
